f48f7fcd5526df7839a3e320259d0decf0a6b9d0
Morgane Julian <morganne.j@live.fr>
2022-03-06 09:50:05 +0100

    Initial Commit

---
+++ MyFluxRSS/MyFluxRSSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFluxRSSApp.swift
+//  MyFluxRSS
+//
+//  Created by Morgane Julian on 06/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyFluxRSSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
